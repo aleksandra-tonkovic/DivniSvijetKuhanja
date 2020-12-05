d6e64563c5a64c6d2482f57e8a8c7aa6d0fe2d53
aleksandra-tonkovic <75422243+aleksandra-tonkovic@users.noreply.github.com>
2020-12-05 13:40:34 +0100

    Create add.php

---
+++ add.php
@@ -0,0 +1,54 @@
+<?php
+
+$host = "sql201.epizy.com";
+$user = "epiz_27078655";
+$password = "YiwKIamwgkUoV";
+$dbname = "epiz_27078655_1";
+
+$con = mysqli_connect($host, $user, $password,$dbname);
+
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+//$input = json_decode(file_get_contents('php://input'),true);
+
+
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+
+switch ($method) {
+    case 'GET':
+      $id = $_GET['id'];
+      $sql = "select * from table1".($id?" where id=$id":''); 
+      break;
+    case 'POST':
+      $id = $_POST["id"];
+      $name = $_POST["name"];
+      
+      $sql = "insert into table1 (id, name) values ('$id', '$name')"; 
+      break;
+}
+
+// run SQL statement
+$result = mysqli_query($con,$sql);
+
+// die if SQL statement failed
+if (!$result) {
+  http_response_code(404);
+  die(mysqli_error($con));
+}
+
+if ($method == 'GET') {
+    if (!$id) echo '[';
+    for ($i=0 ; $i<mysqli_num_rows($result) ; $i++) {
+      echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+    }
+    if (!$id) echo ']';
+  } elseif ($method == 'POST') {
+    echo json_encode($result);
+  } else {
+    echo mysqli_affected_rows($con);
+  }
+
+$con->close();
